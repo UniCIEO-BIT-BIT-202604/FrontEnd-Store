3359b2d8aa68cb600655ecb7dcc7d3f09a28ad9c
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-08-11 19:17:21 -0500

    [+] http-users.ts: Implementa las cabeceras enviando los tokens a cada petición hacia el API

--- src/app/core/services/http-users.ts
+++ src/app/core/services/http-users.ts
@@ -1,33 +1,47 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { HttpAuth } from './http-auth';
 
 @Service()
 export class HttpUsers {
-  // Inyectar una dependencia
+  // Inyectar dependencias
   private http = inject( HttpClient );
+  private authHttp = inject( HttpAuth );
 
   BASE_URL: string = environment.apiUrl;
-  createUser() {}
+
+  private getHeader(): HttpHeaders {
+    const token = this.authHttp.token;
+
+    return new HttpHeaders({
+      'X-Token': token || '',
+      'Content-Type': 'application/json'
+    });
+  }
+
+  createUser( newUser: any ) {
+    return this.http.post<any>( `${ this.BASE_URL }/users`, newUser, { headers: this.getHeader() } );
+  }
 
   getUsers() {
     // Observable (HttpClient)
-    return this.http.get<any>( `${ this.BASE_URL }/users` )
+    return this.http.get<any>( `${ this.BASE_URL }/users`, { headers: this.getHeader() } );
   }
 
   getUserById( id: string | null ) {
     // Observable (HttpClient)
-    return this.http.get<any>( `${ this.BASE_URL }/users/${ id }` );
+    return this.http.get<any>( `${ this.BASE_URL }/users/${ id }`, { headers: this.getHeader() } );
   }
 
   deleteUserById( id: string | null ) {
     // Observable (HttpClient)
-    return this.http.delete( `${ this.BASE_URL }/users/${ id }` );
+    return this.http.delete( `${ this.BASE_URL }/users/${ id }`, { headers: this.getHeader() } );
   }
 
   updateUserById( id: string | null, updatedUser: any ) {
     // Observable (HttpClient)
-    return this.http.patch( `${ this.BASE_URL }/users/${ id }`, updatedUser );
+    return this.http.patch( `${ this.BASE_URL }/users/${ id }`, updatedUser, { headers: this.getHeader() } );
   }
 
 }
